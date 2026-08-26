828d7faafb89243e526a20c693c7218dabcb1792
Chochain Lee <chochain@yahoo.com>
2026-08-27 07:21:38 +0800

    ~/platform/xlvgl.h mv init code to .cpp (MCU constructor sequence is not firm)

--- platform/xlvgl.h
+++ platform/xlvgl.h
@@ -8,22 +8,31 @@
 #include <Wire.h>
 #include <lvgl.h>
 
+#define SCREEN_WIDTH  480
+#define SCREEN_HEIGHT 480
+
+#define TOUCH_SDA  19
+#define TOUCH_SCL  45  
+#define TOUCH_INT  4     // B0, might blip on start up
+#define TOUCH_RST  5     // B1
+
 class LVGLRenderer {
 private:
-    uint32_t _screen_width;
-    uint32_t _screen_height;
-    QueueHandle_t _incoming_vector_queue;
-    TaskHandle_t _task_handle;
+    uint32_t             _screen_width;
+    uint32_t             _screen_height;
+    QueueHandle_t        _incoming_vector_queue;
+    TaskHandle_t         _task_handle;
     
     // 📺 Embedded Arduino_GFX Hardware Display Infrastructure Components
-    Arduino_DataBus*    _bus;
-    ESP32RGBPanel*      _rgbpanel;
-    Arduino_RGB_Display* _display; // Standard class wrapping RGB panel logic
+    Arduino_DataBus      *_bus;
+    ESP32RGBPanel        *_rgbpanel;
+    Arduino_RGB_Display  *_display; // Standard class wrapping RGB panel logic
+    TAMC_GT911           *_ts;
 
     // LVGL internal canvas properties
-    lv_obj_t* _canvas_obj;
-    uint8_t* _canvas_buffer;
-    lv_draw_line_dsc_t _line_dsc;
+    lv_obj_t             *_canvas_obj;
+    uint8_t              *_canvas_buffer;
+    lv_draw_line_dsc_t   _line_dsc;
 
     static void vTaskRenderBridge(void *pvParameters) {
         LVGLRenderer* instance = (LVGLRenderer*)pvParameters;
@@ -36,7 +45,7 @@ private:
     void initHardwarePanel();
 
 public:
-    LVGLRenderer(uint32_t width = 480, uint32_t height = 480) :
+    LVGLRenderer(uint32_t width = SCREEN_WIDTH, uint32_t height = SCREEN_HEIGHT) :
         _screen_width(width),
         _screen_height(height),
         _incoming_vector_queue(NULL),
@@ -44,6 +53,7 @@ public:
         _bus(NULL),
         _rgbpanel(NULL),
         _display(NULL),
+        _ts(
         _canvas_obj(NULL),
         _canvas_buffer(NULL) {}
 
