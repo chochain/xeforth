d30b092452780ff75eb0cb0c7e55519955afc68b
Chochain Lee <chochain@yahoo.com>
2026-08-26 17:00:43 +0800

    ~/platform/lvgl.h add Arduino GFX ref

--- platform/lvgl.h
+++ platform/lvgl.h
@@ -2,31 +2,37 @@
 #define LVGL_RENDERER_H
 
 #include <Arduino.h>
+#include <xlvgl.h>
+#include <Arduino_GFX_Library.h> // Include your standard GFX header
+#include <TAMC_GT911.h>
+#include <Wire.h>
 #include <lvgl.h>
 
 class LVGLRenderer {
 private:
-    uint32_t           _screen_width;
-    uint32_t           _screen_height;
-    QueueHandle_t      _incoming_vector_queue;
-    TaskHandle_t       _task_handle;
+    uint32_t _screen_width;
+    uint32_t _screen_height;
+    QueueHandle_t _incoming_vector_queue;
+    TaskHandle_t _task_handle;
     
-    // LVGL drawing resources
-    lv_obj_t*          _canvas_obj;
-    uint8_t*           _canvas_buffer;
+    // 📺 Embedded Arduino_GFX Hardware Display Infrastructure Components
+    Arduino_DataBus*    _bus;
+    ESP32RGBPanel*      _rgbpanel;
+    Arduino_RGB_Display* _display; // Standard class wrapping RGB panel logic
+
+    // LVGL internal canvas properties
+    lv_obj_t* _canvas_obj;
+    uint8_t* _canvas_buffer;
     lv_draw_line_dsc_t _line_dsc;
 
-    // 🚨 FreeRTOS tasks inside classes MUST be declared as "static void"
     static void vTaskRenderBridge(void *pvParameters) {
-        // Cast the generic void pointer directly back into a class instance context
         LVGLRenderer* instance = (LVGLRenderer*)pvParameters;
         instance->runRenderLoop();
     }
 
-    // This internal worker function handles the actual execution logic on Core 1
     void runRenderLoop();
     
-    // Internal hardware initializer for the 4848S040 screen panel
+    // Internal hardware initialization method
     void initHardwarePanel();
 
 public:
@@ -35,12 +41,14 @@ public:
         _screen_height(height),
         _incoming_vector_queue(NULL),
         _task_handle(NULL),
+        _bus(NULL),
+        _rgbpanel(NULL),
+        _display(NULL),
         _canvas_obj(NULL),
         _canvas_buffer(NULL) {}
 
-    // Allocates memory layers and spins up the FreeRTOS rendering thread on Core 1
     bool begin(QueueHandle_t vector_queue, UBaseType_t task_priority);
 };
 
-#endif
+#endif // LVGL_RENDERER_H
 
