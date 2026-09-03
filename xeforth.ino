1fcaaa8405c69e7537e8775d14c7ec379e7350d7
Chochain Lee <chochain@yahoo.com>
2026-09-02 21:47:53 -0700

    ~/xeforth.ino restore xQueGL (full set)

--- xeforth.ino
+++ xeforth.ino
@@ -36,8 +36,7 @@ const int   WIFI_PORT = 80;               ///< and the password
 // Define structural payload contracts uniformly across your files
 // Instantiate Global Message-Routing Pipelines
 xQueWeb *webToForthQueue  = NULL;
-//xQueGL  *forthToLvglQueue = NULL;
-xQueWeb  *forthToLvglQueue = NULL;
+xQueGL  *forthToLvglQueue = NULL;
 
 // Instantiate the distinct, modular systems with custom parameters
 XServer myWebServer(WIFI_SSID, WIFI_PASS, WIFI_PORT);
@@ -48,9 +47,8 @@ void setup() {
     Serial.begin(115200);
 
     // 1. Build the non-fragmenting communications pipeline channels
-    webToForthQueue  = (xQueWeb*)xQueueCreate(10, sizeof(que_msg_t));
-//    forthToLvglQueue = (xQueGL* )xQueueCreate(50, sizeof(draw_vec_t));
-    forthToLvglQueue = (xQueWeb* )xQueueCreate(50, sizeof(que_msg_t));
+    webToForthQueue  = (xQueWeb*)xQueueCreate(10, sizeof(msg_web_t));  // to use PSRAM, see xQueueCreateWithCaps
+    forthToLvglQueue = (xQueGL* )xQueueCreate(50, sizeof(msg_gl_t));
 
     if (webToForthQueue == NULL || forthToLvglQueue == NULL) {
         Serial.println("Critical: Failed to generate system pipelines.");
