d862429b00dcd29b918fd3103eb3f72b5ccdc381
Chochain Lee <chochain@yahoo.com>
2026-09-09 19:36:50 +0800

    ~/platform/main.cpp msg_raw_t=>msg_web_t

--- platform/main.cpp
+++ platform/main.cpp
@@ -108,7 +108,7 @@ int main(int argc, char *argv[]) {
     };
 
     /* 3. Simulate an HTTP POST action pushing data into the front of the bridge */
-    msg_raw_t req;
+    msg_web_t req;
     for (int i=0; i < (int)(sizeof(cmd)/sizeof(char*)); i++) {
         std::cout << "\nUser: " << cmd[i] << std::endl;
         
