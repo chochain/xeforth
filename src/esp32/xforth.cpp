e83d31fbb1626485364c55a1fa84e4673954b157
Chochain Lee <chochain@yahoo.com>
2026-09-09 22:09:24 -0700

    ~/src/esp32/xforth.cpp mv outer logic into handle_web_req

--- src/esp32/xforth.cpp
+++ src/esp32/xforth.cpp
@@ -24,13 +24,16 @@ bool XForth::begin(xQueWeb *web, xQueUI *ui, int priority) {
 void XForth::handle_web_req() {
     msg_web_t req;
     while (_web->get_req(req)) {
-        Serial.printf("core%d xforth> incoming cmd -> %s\n", _core, (char*)req.buf);
+        char *cmd = (char*)req.buf;
+        Serial.printf("core%d xforth> incoming cmd -> %s\n", _core, cmd);
             
         // Execute non-fragmenting multi-token text processing
-        outer(req.id, (char*)req.buf);
+        forth_vm(cmd, feedback);             /// one-line per call
         
         // CC:Brief safety heartbeat yield hook
     }
+//    msg_web_t rsp = { id, 0x0, true };
+//    _web->put_rsp(rsp);
 }
 
 void XForth::handle_ui_rsp() {
@@ -66,23 +69,16 @@ void XForth::feedback(int len, const char *rst) {
     }
     memcpy(web_rsp.buf, rst, sz);             /// leave last byte to
     web_rsp.buf[sz] = '\0';                   /// ensure \0 terminated
+    web_rsp.eos     = false;
         
     if (!_web->put_rsp(web_rsp)) {
         Serial.printf("xforth web_rsp failed on %s\n", rst);
     }
 }
 
-void XForth::outer(uint32_t id, char *cmd) {
+void XForth::outer(uint32_t id, char *cmd) {  /// not used, call forth_vm directly
     if (cmd == NULL || strlen(cmd) == 0) return;
-
-    forth_vm(cmd, feedback);             /// one-line per call
     
-    msg_web_t rsp = { id, 0x0, true };
-    _web->put_rsp(rsp);
-    
-    return;
-    
-#if 0  /* outer interpreter, without istringstream */
     // Extract the very first token word from the continuous text buffer
     // using the reentrant, thread-safe strtok_r function
     char* save_ptr;
@@ -96,5 +92,4 @@ void XForth::outer(uint32_t id, char *cmd) {
         // Seek out the next individual space-separated command segment
         idiom = strtok_r(NULL, " ", &save_ptr);
     }
-#endif    
 }
