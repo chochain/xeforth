9e48607437b910af982ef0c7f8241509662926e4
Chochain Lee <chochain@yahoo.com>
2026-09-20 15:13:57 +0800

    ~/src/esp32/mcu_actor.h tune mem_stat

--- src/esp32/mcu_actor.h
+++ src/esp32/mcu_actor.h
@@ -17,8 +17,7 @@ const char *APP_VERSION = "xeForth v1.0";
 ///
 ///> interface to core module
 ///
-#include "xactor.h"
-#include "xserver2.h"             ///< ESP32 Async Web Server (Gate)
+#include "xserver2.h"            ///< ESP32 Async Web Server (Gate)
 #include "xforth_actor.h"        ///< Forth VM Actor
 #include "xgl_actor.h"           ///< GUI Actor (LVGL+Touch)
 #include "../ceforth.h"          ///< Forth VM itself
@@ -33,10 +32,10 @@ extern List<Code*, E4_DICT_SZ> dict;
 void mem_stat()  {
     size_t  rf = heap_caps_get_free_size(MALLOC_CAP_INTERNAL);
     size_t  rt = heap_caps_get_total_size(MALLOC_CAP_INTERNAL);
-    int64_t rp = 1000L * rf / rt;
+    int64_t rp = (int64_t)(1000.0f * rf / rt);
     size_t  sf = heap_caps_get_free_size(MALLOC_CAP_SPIRAM);
     size_t  st = heap_caps_get_total_size(MALLOC_CAP_SPIRAM);
-    int64_t sp = 1000L * sf / st;
+    int64_t sp = (int64_t)(1000.0f * sf / st);
     
     LOGS("\nSRAM ");  LOG(static_cast<float>(rp) * 0.1);
     LOGS("% free ("); LOG(rf>>10);
@@ -105,6 +104,8 @@ void mcu_init() {
     
     const int sz = (int)(sizeof(ops))/(sizeof(Code));
     for (const Code &c : ops) dict.push((Code*)&c);
+
+    mem_stat();
 }
 #endif // _MCU_ACTOR_H
 
